ae9ff94f4c6f87d2cd98c52fdb38219695ae3391
Yu-Ming Chen <yminchen@umich.edu>
2020-08-30 15:15:36 -0400

    Update sync scipt

--- test/sync_hw2.py
+++ test/sync_hw2.py
@@ -2,49 +2,56 @@
 Synchronize homework files (either save files to google drive or copy files from github/google drive) 
 """
 from shutil import copyfile
-from urllib2 import urlopen
+# from urllib2 import urlopen
+import wget
 
-def download(url, file_path):
-    response = urlopen(url, file_path)
-    data = response.read()
-    txt_str = str(data)
-    lines = txt_str.split("\\n")
-    fx = open(file_path,"w")
-    for line in lines:
-        fx.write(line+ "\n")
-    fx.close()
+# def download(url, file_path):
+#     response = urlopen(url, file_path)
+#     data = response.read()
+#     txt_str = str(data)
+#     lines = txt_str.split("\\n")
+#     fx = open(file_path,"w")
+#     for line in lines:
+#         fx.write(line+ "\n")
+#     fx.close()
 
 def sync_hw2():
   if 'google.colab' in str(get_ipython()):
     from google.colab import drive
     drive.mount('/content/drive')
 
+    import os
     from os import path
     # Save files from Colab to Google drive
     if path.exists("quadrotor.py"):
-        # !cp quadrotor.py  drive/My\ Drive/MEAM517_colab/hw2/quadrotor.py 
-        # !cp quadrotor_generator.py drive/My\ Drive/MEAM517_colab/hw2/quadrotor_generator.py 
-        # !cp quad_sim.py drive/My\ Drive/MEAM517_colab/hw2/quad_sim.py 
-        # !cp trajectories.py drive/My\ Drive/MEAM517_colab/hw2/trajectories.py 
-        copyfile("quadrotor.py",  "drive/My Drive/MEAM517_colab/hw2/quadrotor.py")
-        copyfile("quadrotor_generator.py", "drive/My Drive/MEAM517_colab/hw2/quadrotor_generator.py")
-        copyfile("quad_sim.py", "drive/My Drive/MEAM517_colab/hw2/quad_sim.py")
-        copyfile("trajectories.py", "drive/My Drive/MEAM517_colab/hw2/trajectories.py")
+      if not path.exists("drive/My Drive/MEAM517_colab/hw2"):
+        # create hw folder in google drive
+        # !mkdir drive/My\ Drive/MEAM517_colab/hw2
+        os.mkdir('drive/My Drive/MEAM517_colab/hw2')
+      # !cp quadrotor.py  drive/My\ Drive/MEAM517_colab/hw2/quadrotor.py 
+      # !cp quadrotor_generator.py drive/My\ Drive/MEAM517_colab/hw2/quadrotor_generator.py 
+      # !cp quad_sim.py drive/My\ Drive/MEAM517_colab/hw2/quad_sim.py 
+      # !cp trajectories.py drive/My\ Drive/MEAM517_colab/hw2/trajectories.py 
+      copyfile("quadrotor.py",  "drive/My Drive/MEAM517_colab/hw2/quadrotor.py")
+      copyfile("quadrotor_generator.py", "drive/My Drive/MEAM517_colab/hw2/quadrotor_generator.py")
+      copyfile("quad_sim.py", "drive/My Drive/MEAM517_colab/hw2/quad_sim.py")
+      copyfile("trajectories.py", "drive/My Drive/MEAM517_colab/hw2/trajectories.py")
     # Read files to Colab
     else:
       # If the hw folder doesn't exist in google drive, create the folder and create new homework scripts
       if not path.exists("drive/My Drive/MEAM517_colab/hw2"):
         # create hw folder in google drive
-        !mkdir drive/My\ Drive/MEAM517_colab/hw2
+        # !mkdir drive/My\ Drive/MEAM517_colab/hw2
+        os.mkdir('drive/My Drive/MEAM517_colab/hw2')
         # download homework scirpts to colab 
         # !curl -s https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quadrotor.py > quadrotor.py
         # !curl -s https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quadrotor_generator.py > quadrotor_generator.py
         # !curl -s https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quad_sim.py > quad_sim.py
         # !curl -s https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/trajectories.py > trajectories.py
-        !download("https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quadrotor.py", "quadrotor.py")
-        !download("https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quadrotor_generator.py", "quadrotor_generator.py")
-        !download("https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quad_sim.py", "quad_sim.py")
-        !download("https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/trajectories.py", "trajectories.py")
+        wget.download("https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quadrotor.py", "quadrotor.py")
+        wget.download("https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quadrotor_generator.py", "quadrotor_generator.py")
+        wget.download("https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/quad_sim.py", "quad_sim.py")
+        wget.download("https://raw.githubusercontent.com/yminchen/MEAM517_Colab/test/test/trajectories.py", "trajectories.py")
         # copy the new scripts to hw folder in google drive
         # !cp quadrotor.py drive/My\ Drive/MEAM517_colab/hw2/quadrotor.py
         # !cp quadrotor_generator.py drive/My\ Drive/MEAM517_colab/hw2/quadrotor_generator.py
